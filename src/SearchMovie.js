d50a6e7dc8947bc6b62d13296e3f20b1f349db0d
karantondare <karantondare.study@gmail.com>
2020-12-06 19:33:06 +0530

    first commit

---
+++ src/SearchMovie.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import MovieCard from './MovieCard';
+
+export default function SearchMovie() {
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  const SearchMovies = async (e) => {
+    e.preventDefault();
+    console.log('submitting');
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=da128f0f40bdeb2660ba6801fe504117&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <form className="form" onSubmit={SearchMovies}>
+        <label className="label" htmlFor="query">
+          Movie Name:
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          placeholder="search a movie"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="button">Search</button>
+      </form>
+      <div className="card-list">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+      </div>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SearchMovie from './SearchMovie';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">React Movie Search</h1>
+        <SearchMovie />
+      </div>
+    );
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Main />, document.getElementById('root'));
